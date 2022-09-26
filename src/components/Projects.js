560ec76c62dedd4e2f50b91da7f483606effd154
Arik <arikabdullah1122@gmail.com>
2022-09-26 19:37:46 -0400

    K

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -36,7 +36,7 @@ export const projects = [
 		id: 3,
 		key: 3,
 		title: "Noter",
-		img: "./images/BoudoirXAlyssa.png",
+		img: "./images/Noter.png",
 		content:
 			"Noter is a browser based note taking application I built using React and TypeScript. It is complete with Light/Dark themes using useContext and useEffect hooks to set notes in your browsers storage. Your notes stay yours.",
 		tech: "HTML, CSS, JavaScript, Node.js",
